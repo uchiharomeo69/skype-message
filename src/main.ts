4dafbcf0eae043812f41d19561fc436f557d9ac8
Your Name <uchiharomeo69@gmail.com>
2021-09-30 21:00:06 +0700

    aa

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.PORT || 3003);
   console.log('success cmnr');
 }
